6c6dd3ecffaac761876ebf1ff7886267741abba6
makopi23 <kipersonalpc@gmail.com>
2016-05-28 13:35:37 +0900

    CommentList and CommentForm

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -1,9 +1,32 @@
+// tutorial2.js
+var CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+var CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
 // tutorial1.js
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
@@ -12,3 +35,4 @@ ReactDOM.render(
   <CommentBox />,
   document.getElementById('content')
 );
+
